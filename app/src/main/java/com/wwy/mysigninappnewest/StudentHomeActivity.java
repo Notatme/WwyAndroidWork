81126b6f00c102401a2599181b630384d33b9dde
Notatme <2371106661@qq.com>
2020-10-29 11:53:27 +0800

    合并功能，已经整理了业务逻辑，需要罗列学生信息,个人信息,最新

--- app/src/main/java/com/wwy/mysigninappnewest/StudentHomeActivity.java
+++ app/src/main/java/com/wwy/mysigninappnewest/StudentHomeActivity.java
@@ -14,11 +14,18 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.tabs.TabLayout;
 import com.king.zxing.Intents;
+import com.wwy.mysigninappnewest.impl.Data;
+import com.wwy.mysigninappnewest.impl.ZhuYeActivity;
+import com.wwy.mysigninappnewest.pojo.Student;
+
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
 public class StudentHomeActivity extends AppCompatActivity {
     ActionBar actionBar;
     // 底部导航栏
     private TabLayout tabLayout;
+    private String ObId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +40,13 @@ public class StudentHomeActivity extends AppCompatActivity {
 
     protected void initTab() {
         tabLayout = findViewById(R.id.nav_bar_student);
+//<<<<<<< Updated upstream
         // 添加tab切换事件
+//=======
+       // btn_scan_qrcode
+
+
+//>>>>>>> Stashed changes
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -93,15 +106,31 @@ public class StudentHomeActivity extends AppCompatActivity {
     // 由于fragment不能接受activity传回的结果
     // 二维码传回的值由fragment附在的activity接受
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {      //扫描二维码返回的结果
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && data != null) {
             switch (requestCode) {
                 case 1:
                     // 获取二维码扫描的值
                     String result = data.getStringExtra(Intents.Scan.RESULT);
-                    Toast.makeText(StudentHomeActivity
-                            .this, result, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(StudentHomeActivity.this, "签到成功", Toast.LENGTH_SHORT).show();
+                    Student student=new Student();
+                    student.setQiandaocode(result);
+                    student.update(Data.getObjectId(), new UpdateListener() {     //把签到码加入到数据库中
+
+                        @Override
+                        public void done(BmobException e) {
+                            if(e==null){
+                                Toast.makeText(StudentHomeActivity.this, "签到成功！", Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(StudentHomeActivity.this, "签到失败！", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                    });
+
+                    //Toast.makeText(StudentHomeActivity.this, "验证码正确，签到成功！", Toast.LENGTH_LONG).show();
+
                     break;
             }
         }
--- app/src/main/java/com/wwy/mysigninappnewest/TeacherFragmentCreate.java
+++ app/src/main/java/com/wwy/mysigninappnewest/TeacherFragmentCreate.java
@@ -7,14 +7,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.wwy.mysigninappnewest.impl.Data;
+import com.wwy.mysigninappnewest.impl.SetSignActivity;
+import com.wwy.mysigninappnewest.jiemian.NumberActivity;
+import com.wwy.mysigninappnewest.pojo.Teacher;
+
+import java.util.UUID;
+
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
+
 public class TeacherFragmentCreate extends Fragment {
     Activity context;
     Button createQRCodeBtn;
+    private Button createNumberBtn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,15 +38,61 @@ public class TeacherFragmentCreate extends Fragment {
         super.onActivityCreated(savedInstanceState);
         context = getActivity();
         createQRCodeBtn = (Button) context.findViewById(R.id.btn_create_qrcode);
+        createNumberBtn=(Button) context.findViewById(R.id.btn_create_Numbercode);
         createQRCodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+ //Updated upstream
                 // 添加点击事件跳转二维码生成页
-                Intent intent = new Intent(context, GenerateQRCodeActivity.class);
+                //Intent intent = new Intent(context, GenerateQRCodeActivity.class);
                 // 传递给生成页的字符串信息
-                intent.putExtra("QRCodeInfo", "Hello,World");
+                //intent.putExtra("QRCodeInfo", "Hello,World");
+
+
+                Data.setQRCode(UUID.randomUUID().toString().replace("-", ""));     //使用UUID生成随机字符串
+                Teacher teacher=new Teacher();
+                teacher.setTqiandaocode(Data.getQRCode());
+                teacher.update(Data.getObjectId(), new UpdateListener() {     //把签到码加入到数据库中
+
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                           // Toast.makeText(SetSignActivity.this, "设置签到码成功！", Toast.LENGTH_LONG).show();
+                           // Intent seccess=new Intent();
+                            //seccess.setClass(SetSignActivity.this, NumberActivity.class);
+                           // Bundle bundle = new Bundle();     //简单参数传递objectId
+                            //bundle.putString("number",CCode);
+                            //seccess.putExtras(bundle);
+                            //startActivity(seccess);
+                            //SetSignActivity.this.finish();
+                            Intent intent = new Intent(context, GenerateQRCodeActivity.class);
+                            intent.putExtra("QRCodeInfo", Data.getQRCode());
+                            startActivity(intent);
+                            //Toast.makeText(TeacherFragmentCreate.this, "设置签到码失败！", Toast.LENGTH_LONG).show();
+
+                        }else{
+                           // Toast.makeText(TeacherFragmentCreate.this, "设置签到码失败！", Toast.LENGTH_LONG).show();
+                           // Toast.makeText(TeacherFragmentCreate.this, "帐号或密码有误", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+
+
+
+
+
+            }
+        });
+        createNumberBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SetSignActivity.class);
+               // intent.putExtra("QRCodeInfo", "Hello,World");
+//>>>>>>> Stashed changes
                 startActivity(intent);
             }
         });
+
     }
 }
